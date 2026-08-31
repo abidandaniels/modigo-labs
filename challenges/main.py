fdbcececd0b3a2810456372a71b416750895edf9
Abidan Daniels <abidandaniels@gmail.com>
2026-08-31 16:25:45 +0100

    Complete: Multiplication Table

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,7 @@
-def build_countdown(start):
-    countdown = []
-    for i in range (start, 0 , -1) :
-        countdown.append(i)
-
-    # TODO: use a for loop with range() to count down from `start` to 1,
-    # appending each number to `countdown`
-    return countdown
+def multiplication_table(number, limit):
+    table = []
+    # TODO: use a for loop to build a list of number * 1 through number * limit
+    for i in range (1, limit +1):
+       
+        table.append (number *i)
+    return table
