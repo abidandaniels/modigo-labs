e75bd9c6d5cd8996b3d40d25753789f7857bb4d8
Abidan Daniels <abidandaniels@gmail.com>
2026-08-31 15:06:40 +0100

    Complete: Build a Countdown

--- challenges/main.py
+++ challenges/main.py
@@ -1,3 +1,8 @@
-def double_values(numbers):
-  
-    return [num *2 for num in numbers]
+def build_countdown(start):
+    countdown = []
+    for i in range (start, 0 , -1) :
+        countdown.append(i)
+
+    # TODO: use a for loop with range() to count down from `start` to 1,
+    # appending each number to `countdown`
+    return countdown
