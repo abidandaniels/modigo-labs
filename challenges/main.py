ef2dc95a28be2705298aece9e6142a84f26acb4e
Abidan Daniels <abidandaniels@gmail.com>
2026-09-02 11:53:53 +0100

    Complete: Second Largest Number

--- challenges/main.py
+++ challenges/main.py
@@ -1,7 +1,7 @@
-def find_common_elements(list1, list2):
-    # TODO: use for loops to find values present in both list1 and list2, with no duplicates
-    common_elements = []
-    for item in list1:
-        if item in list2 and item not in common_elements:
-            common_elements.append (item)
-    return common_elements
+def second_largest(numbers):
+   
+     
+    # TODO: return the second largest DISTINCT number in `numbers`
+    unique_numbers = set(numbers)
+    unique_numbers.remove(max(unique_numbers))
+    return max (unique_numbers)
