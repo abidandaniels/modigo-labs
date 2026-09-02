5667225c4cb7d1a4647422607adddd01ffa56528
Abidan Daniels <abidandaniels@gmail.com>
2026-09-02 12:38:07 +0100

    Complete: Below is a function meant to calculate the perimeter of a re

---
+++ labs/main.py
@@ -0,0 +1,5 @@
+def calculate_perimeter(length, width):
+    # TODO: store 2 * (length + width) in a variable called perimeter
+    perimeter = 2 * (length + width) # TODO: replace 0 with the correct calculation
+
+    return perimeter
