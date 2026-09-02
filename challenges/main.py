75781fe2c0c95355152cf25850177d3c451b0ee9
Abidan Daniels <abidandaniels@gmail.com>
2026-09-02 09:32:00 +0100

    Complete: Common Elements Finder

--- challenges/main.py
+++ challenges/main.py
@@ -1,7 +1,7 @@
-def multiplication_table(number, limit):
-    table = []
-    # TODO: use a for loop to build a list of number * 1 through number * limit
-    for i in range (1, limit +1):
-       
-        table.append (number *i)
-    return table
+def find_common_elements(list1, list2):
+    # TODO: use for loops to find values present in both list1 and list2, with no duplicates
+    common_elements = []
+    for item in list1:
+        if item in list2 and item not in common_elements:
+            common_elements.append (item)
+    return common_elements
