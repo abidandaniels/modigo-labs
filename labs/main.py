d0d1f843355e21d29b71a54b9355fa4721fcb17d
Abidan Daniels <abidandaniels@gmail.com>
2026-09-02 13:25:35 +0100

    Complete: Write a function that calculates Body Mass Index (BMI) from

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,14 @@
-def swap_values(a, b):
-    new_a = b
-    new_b = a
-    return (new_a, new_b)
+def bmi_report(weight_kg, height_m):
+    # TODO: calculate bmi, round it to 1 decimal place, determine the category,
+    # and return "BMI: {bmi}, Category: {category}"
+    category = ""
+    bmi = round((weight_kg / (height_m ** 2)), 1)
+    if bmi < 18.5 :
+        category = "Underweight"
+    elif 18.5 <= bmi <= 24.9 :
+        category = "Normal weight"
+    elif 25.0 <= bmi <= 29.9 :
+        category = "Overweight"
+    else :
+        category = "Obese"
+    return (f"BMI: {bmi}, Category: {category}")
