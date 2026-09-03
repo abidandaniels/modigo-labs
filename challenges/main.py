7c651a305f0d076d6cecea5d6c816eb9cb2d8630
Abidan Daniels <abidandaniels@gmail.com>
2026-09-03 09:23:58 +0100

    Complete: Prime Number Checker

--- challenges/main.py
+++ challenges/main.py
@@ -1,5 +1,14 @@
-def calculate_perimeter(length, width):
-    # TODO: store 2 * (length + width) in a variable called perimeter
-    perimeter = 2 * (length + width)  # TODO: replace 0 with the correct calculation
-
-    return perimeter
+def is_prime(number):
+    # TODO: return True if `number` is prime, False otherwise
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0 :
+        return False 
+    i = 5
+    while i * i <= number :
+        if number % i == 0 or number % (i + 2) == 0 :
+            return False 
+        i += 6
+    return True
