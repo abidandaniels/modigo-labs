b9680b48bd5241a39d556c38bede153dd55e75a9
Abidan Daniels <abidandaniels@gmail.com>
2026-09-03 13:41:32 +0100

    Complete: Fix the Character Counter

--- challenges/main.py
+++ challenges/main.py
@@ -1,15 +1,6 @@
-def smart_title_case(sentence):
-    # TODO: capitalize each word except connector words (a, an, the, of, in, on, and),
-    # unless that connector word is the first word in the sentence
-    if not sentence:
-        return ""
-    connector = {"a", "an", "the", "of", "in", "on", "and"}
-    words = sentence.split(" ")
-    result = []
-    for i, word in enumerate(words):
-        lower_word = word.lower()
-        if i == 0 or lower_word not in connector:
-            result.append(word.capitalize())
-        else:
-            result.append(lower_word)
-    return " ".join(result)
+def count_character(text, target):
+    count = 0
+    for char in text:
+        if char == target:
+            count += 1
+    return count
