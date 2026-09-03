17ec92addb449e45615ad50f83431470c0110e1c
Abidan Daniels <abidandaniels@gmail.com>
2026-09-03 10:31:08 +0100

    Complete: Title Case Converter

--- challenges/main.py
+++ challenges/main.py
@@ -1,4 +1,15 @@
-def swap_values(a, b):
-    new_a = b
-    new_b = a
-    return (new_a, new_b)
+def smart_title_case(sentence):
+    # TODO: capitalize each word except connector words (a, an, the, of, in, on, and),
+    # unless that connector word is the first word in the sentence
+    if not sentence:
+        return ""
+    connector = {"a", "an", "the", "of", "in", "on", "and"}
+    words = sentence.split(" ")
+    result = []
+    for i, word in enumerate(words):
+        lower_word = word.lower()
+        if i == 0 or lower_word not in connector:
+            result.append(word.capitalize())
+        else:
+            result.append(lower_word)
+    return " ".join(result)
