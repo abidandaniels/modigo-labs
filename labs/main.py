5c9882a3cc1ced7bff1960bb728f85cbf3c7729b
Abidan Daniels <abidandaniels@gmail.com>
2026-09-11 13:01:02 +0100

    Complete: Write a function that raises a number to a power. If no powe

--- labs/main.py
+++ labs/main.py
@@ -1,5 +1,3 @@
-def format_line_item(description, amount, tax_rate=7.5, currency="$"):
-    # TODO: calculate the taxed total using tax_rate, round to 2 decimal places,
-    # and return "{description}: {currency}{total}"
-    total = round (amount + (amount * tax_rate / 100), 2 )
-    return (f"{description}: {currency}{total}")
+def power_of(base, exponent=2):
+    # TODO: return base raised to the power of exponent
+    return base ** exponent
